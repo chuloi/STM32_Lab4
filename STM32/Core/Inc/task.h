336a6ba08a5f5dd73a6045ce896dfc85d101a82b
chuloi <chuloicvlcvl190302@gmail.com>
2023-12-03 14:43:31 +0700

    first commit

---
+++ STM32/Core/Inc/task.h
@@ -0,0 +1,10 @@
+#ifndef INC_TASK_H_
+#define INC_TASK_H_
+
+void task_display1();
+void task_display2();
+void task_display3();
+void task_display4();
+void task_display5();
+
+#endif /* INC_TASK_H_ */
